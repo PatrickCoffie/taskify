701028351ffcdfbf9272fcd78a8e0b0af9ff8a1f
Patrick Coffie <pcoffiej1@gmail.com>
2026-06-14 20:12:21 +0000

    fix ALLOWED_HOSTS render issue

--- taskify/settings.py
+++ taskify/settings.py
@@ -23,9 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a%!amrh8ox!3!pzn5*=h#u9=ze3irv!%023fr@*ui#l%bbx^hs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "taskify-om8u.onrender.com",
+    "taskify-gapq.onrender.com",
+    "localhost",
+    "127.0.0.1"
+]
 
 
 # Application definition
@@ -116,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
